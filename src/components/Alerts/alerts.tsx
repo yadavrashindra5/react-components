f7b51c4534ef4fd9a5691ea7a2400e0d61975751
Divyue30597 <tomchi.sharma305@outlook.com>
2024-02-27 18:23:52 +0530

    some additional changes

---
+++ src/components/Alerts/alerts.tsx
@@ -0,0 +1,27 @@
+import Box from "../Box/box";
+
+export default function Alerts() {
+  const colorObj = [
+    {
+      id: 1,
+      color: "#4da1ff",
+      backgroundColor: "#94c7ff",
+    },
+    {
+      id: 2,
+      color: "#4da1ff",
+      backgroundColor: "#94c7ff",
+    },
+    {
+      id: 3,
+      color: "#4da1ff",
+      backgroundColor: "#94c7ff",
+    },
+    {
+      id: 4,
+      color: "#4da1ff",
+      backgroundColor: "#94c7ff",
+    },
+  ];
+  return <Box className={}></Box>
+}
