0e48a5081ac29225677627ec1dc49daa6592c2f5
Divyue30597 <tomchi.sharma305@outlook.com>
2024-03-02 18:30:18 +0530

    done with the assignment, need to update the accessibility

--- src/components/Table/table.tsx
+++ src/components/Table/table.tsx
@@ -272,6 +272,204 @@ export default function Table() {
           </tbody>
         </table>
       </div>
+      <div className={styles.solar_system}>
+        <table>
+          <caption>
+            Data about the planets of our solar system (Planetary facts taken
+            from{" "}
+            <a
+              target="_blank"
+              href="http://nssdc.gsfc.nasa.gov/planetary/factsheet/"
+            >
+              Nasa's Planetary Fact Sheet - Metric
+            </a>
+            ).
+          </caption>
+          <colgroup>
+            <col />
+            <col />
+            <col style={{ border: "0.2rem solid #000" }} />
+          </colgroup>
+          <thead>
+            <tr>
+              <th colSpan={2}></th>
+              <th>Name</th>
+              <th>
+                Mass (10<sup>24</sup>kg)
+              </th>
+              <th>Diameter (km)</th>
+              <th>
+                Density (kg/m<sup>3</sup>)
+              </th>
+              <th>
+                Gravity (m/s<sup>2</sup>)
+              </th>
+              <th>Length of day (hours)</th>
+              <th>
+                Distance from Sun (10<sup>6</sup>km)
+              </th>
+              <th>Mean temperature (°C)</th>
+              <th>Number of moons</th>
+              <th>Notes</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <th rowSpan={5} colSpan={2}>
+                Terrestrial planets
+              </th>
+            </tr>
+            <tr>
+              <th>Mercury</th>
+              <td>0.330</td>
+              <td>4,879</td>
+              <td>5427</td>
+              <td>3.7</td>
+              <td>4222.6</td>
+              <td>57.9</td>
+              <td>167</td>
+              <td>0</td>
+              <td>Closest to the Sun</td>
+            </tr>
+            <tr>
+              <th>Venus</th>
+              <td>4.87</td>
+              <td>12,104</td>
+              <td>5243</td>
+              <td>8.9</td>
+              <td>2802.0</td>
+              <td>108.2</td>
+              <td>464</td>
+              <td>0</td>
+            </tr>
+            <tr>
+              <th>Earth</th>
+              <td>5.97</td>
+              <td>12,756</td>
+              <td>5514</td>
+              <td>9.8</td>
+              <td>24.0</td>
+              <td>149.6</td>
+              <td>15</td>
+              <td>1</td>
+              <td>Our World</td>
+            </tr>
+            <tr>
+              <th>Mars</th>
+              <td>0.642</td>
+              <td>6,792</td>
+              <td>3933</td>
+              <td>3.7</td>
+              <td>24.7</td>
+              <td>229.9</td>
+              <td>-65</td>
+              <td>2</td>
+              <td>The Red Planet</td>
+            </tr>
+            <tr>
+              <th colSpan={1} rowSpan={5}>
+                Jovian Planet
+              </th>
+            </tr>
+            <tr>
+              <th colSpan={1} rowSpan={2}>
+                Gas Giants
+              </th>
+              <th>Jupiter</th>
+              <td>1898</td>
+              <td>142,984</td>
+              <td>1326</td>
+              <td>23.1</td>
+              <td>9.9</td>
+              <td>778.6</td>
+              <td>-110</td>
+              <td>67</td>
+              <td>The Largest Planet</td>
+            </tr>
+            <tr>
+              <th>Saturn</th>
+              <td>586</td>
+              <td>120,536</td>
+              <td>687</td>
+              <td>9.0</td>
+              <td>10.7</td>
+              <td>1433.5</td>
+              <td>-140</td>
+              <td>62</td>
+              <td></td>
+            </tr>
+            <tr>
+              <th colSpan={1} rowSpan={2}>
+                Ice Giants
+              </th>
+              <th>Uranus</th>
+              <td>86.8</td>
+              <td>51,118</td>
+              <td>1271</td>
+              <td>8.7</td>
+              <td>17.2</td>
+              <td>2872.5</td>
+              <td>-195</td>
+              <td>27</td>
+              <td></td>
+            </tr>
+            <tr>
+              <th>Neptune</th>
+              <td>102</td>
+              <td>49,528</td>
+              <td>1638</td>
+              <td>11.0</td>
+              <td>16.1</td>
+              <td>4495.1</td>
+              <td>-200</td>
+              <td>14</td>
+              <td></td>
+            </tr>
+            <tr>
+              <th rowSpan={1} colSpan={2}>
+                Dwarf Planets
+              </th>
+              <th>Pluto</th>
+              <td>0.0146</td>
+              <td>2,370</td>
+              <td>2095</td>
+              <td>0.7</td>
+              <td>153.3</td>
+              <td>5906.4</td>
+              <td>-225</td>
+              <td>5</td>
+              <td>
+                Declassified as a planet in 2006, but this{" "}
+                <a
+                  target="_blank"
+                  href="http://www.usatoday.com/story/tech/2014/10/02/pluto-planet-solar-system/16578959/"
+                >
+                  remains controversial
+                </a>
+                .
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
     </>
   );
+}
+
+interface IRootTable {
+  caption: string;
+  children: React.ReactNode;
+}
+
+function RootTable({ caption, children }: IRootTable) {
+  return (
+    <table>
+      <caption>{caption}</caption>
+      {children}
+    </table>
+  );
+}
+
+function TableHead() {
+  return <thead></thead>;
 }
